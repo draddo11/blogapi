07b20eba5043886a2e1271f099eabb930435e12a
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-12-30 09:55:07 +0000

    author created

--- src/main/java/com/nana/springblog/model/Author.java
+++ src/main/java/com/nana/springblog/model/Author.java
@@ -1,4 +1,67 @@
 package com.nana.springblog.model;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name = "id", nullable = false)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String  email;
+    private String phoneNumber;
+
+    public Author(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Author() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phonenumber) {
+        this.phoneNumber = phonenumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
