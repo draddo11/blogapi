259de42ed85cc18be941492d50b9280441913b49
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-12-30 14:03:27 +0000

    updating db with author details

--- src/main/java/com/nana/springblog/SpringblogApplication.java
+++ src/main/java/com/nana/springblog/SpringblogApplication.java
@@ -1,13 +1,28 @@
 package com.nana.springblog;
 
+import com.nana.springblog.model.Author;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringblogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringblogApplication.class, args);
+
+        @Bean
+                public CommandLineRunner run
+
+
+    }
+
+    private void insertJavaAdvocates(AuthorRepository repository){
+        repository.save(new Author("John" , "Maxwell","johnmax@authors.com","+1 0082625372"));
+        repository.save(new Author("Mike" , "Nelson","mikenelson@authors.com","+011 082625372"));
+        repository.save(new Author("Nick" , "Cage","NickCage@authors.com","+233 2625372"));
+        repository.save(new Author("Chris" , "Tucker","christucker@authors.com","+234 09975372"));
     }
 
 }
