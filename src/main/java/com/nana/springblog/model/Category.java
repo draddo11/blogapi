0092ab8d44c323dee8a23e1dd7ef8c97be2eed37
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-12-30 10:21:19 +0000

    category done amd updated with tostring()

--- src/main/java/com/nana/springblog/model/Category.java
+++ src/main/java/com/nana/springblog/model/Category.java
@@ -1,4 +1,66 @@
 package com.nana.springblog.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String categoryName;
+    private String text;
+    private String Categories;
+
+    public Category(String categoryName, String text, String categories) {
+        this.categoryName = categoryName;
+        this.text = text;
+        Categories = categories;
+    }
+
+    public Category() {
+
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCategories() {
+        return Categories;
+    }
+
+    public void setCategories(String categories) {
+        Categories = categories;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                ", text='" + text + '\'' +
+                ", Categories='" + Categories + '\'' +
+                '}';
+    }
 }
